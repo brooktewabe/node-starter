4c85610cceddfa3e5417a382509cfcdfed06e2b2
brooktewabe <brooktewabe00@gmail.com>
2024-03-05 16:34:50 +0300

    'passport-jwt-route-auth'

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -2,12 +2,12 @@ const express = require("express");
 const {blogController} =require("../controller");
 const validate = require("../middlewares/validate")
 const {blogValidation} = require ("../validations")
-
-const router = express.Router();
-
-router.get('/blogs',blogController.getBlogs);
+const auth = require('../middlewares/auth')
+const router = express.Router()
+router.get('/blogs',auth,blogController.getBlogs);
 router.post(
     '/blog',
+    auth,
     validate(blogValidation.createBlogSchema),
     blogController.createBlog
 );
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const {errorHandler,errorConverter} = require("./middlewares/error")
 const ApiError = require( "./utils/ApiError");
 const httpStatus = require( 'http-status');
 const morgan = require("./config/morgan");
+const passport = require('passport')
+const {jwtStrategy} = require('./config/passport')
 const express = require('express')
 const app=express()
 
@@ -11,8 +13,12 @@ const app=express()
 app.use(express.json())
 app.use(morgan.successHandler)
 app.use(morgan.errorHandler)
+// jwt auth
+app.use(passport.initialize());
+passport.use("jwt", jwtStrategy);
 app.use(blogRouter)
 app.use(authRouter)
+// path not found
 app.use((req,res,next)=>{
     next(new ApiError(httpStatus.NOT_FOUND, "404 Not found"))
 })
