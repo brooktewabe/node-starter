839cdb177466f05a7e686148b1ed99a6401b3439
brooktewabe <brooktewabe00@gmail.com>
2024-03-07 10:44:47 +0300

    'brute-force-rate-limiter'

--- config/config.js
+++ config/config.js
@@ -10,11 +10,17 @@ if (error) {
 module.exports = {
     port: envVars.PORT,
     dbConnection: envVars.DB_CONNECTION,
+    dbName: envVars.DB_NAME,
     env: envVars.NODE_ENV,
     jwt:{
         secret: envVars.JWT_SECRET,
         accessExpirationMinutes:envVars.JWT_ACCESS_EXPIRATION_MINUTES,
         refreshExpirationDays:envVars.JWT_REFRESH_EXPIRATION_DAYS,
-    }
+    },
+    rateLimiter: {
+      maxAttemptPerDay: envVars.MAX_ATTEMPT_PER_DAY,
+      maxAttemptByIpUsername: envVars.MAX_ATTEMPT_BY_IP_USERNAME,
+      maxAttemptPerEmail: envVars.MAX_ATTEMPT_PER_EMAIL,
+    },
 
 };
--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -14,7 +14,7 @@ const register =catchAsync(async (req, res) => {
 
 const login = catchAsync(async (req, res) => {
     const { email, password } = req.body;
-    const user = await authService.login(email, password);
+    const user = await authService.login(email, password,req.connection.remoteAddress);
     // generate token
     const tokens = await tokenService.generateAuthTokens(user.id);
     res.status(httpStatus.OK).send({ user, tokens });
